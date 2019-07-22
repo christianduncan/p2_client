b7aa9aacb17eba5d9ffeebf10ac6ddae9568c052
Christian Duncan <christianeduncan3@gmail.com>
2019-07-22 15:25:27 -0400

    rendering extra details

--- src/components/AnimalDetail.js
+++ src/components/AnimalDetail.js
@@ -14,7 +14,15 @@ class AnimalDetail extends Component {
             <div>
                 <h2>{this.props.animal.name}</h2>
                 <img src={this.props.animal.imageURL} alt="" />
+                <p>Breed(size): {this.props.animal.breed} 
+                 ({this.props.animal.size})</p>
+                <p>Age: {this.props.animal.age}-{this.props.animal.gender}</p>
                 <p>Description: {description}</p>
+                <h4> To adopt {this.props.animal.name} please reference contact information below!</h4>
+                <p>Email: {this.props.animal.email}</p>
+                <p>Phone: {this.props.animal.phone}</p>
+                <p>{this.props.animal.name} is waiting for you at {this.props.animal.address1} in {this.props.animal.city},{this.props.animal.state}</p>
+                <h1>🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾</h1>
                 
             </div>
         )
